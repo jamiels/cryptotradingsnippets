7ca98e56e1e0763749d77a8470d97ac137410a0f
Jamiel Sheikh <i@jamiel.net>
2018-05-29 02:39:12 -0400

    Create snippets.py

---
+++ snippets.py
@@ -0,0 +1,10 @@
+# Used in exercise 1
+pizza_tx_hash = "a1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d"
+
+pizza_block_hash = "00000000152340ca42227603908689183edc47355204e7aca59383b0aaac1fd8"
+
+
+laszlo_addr = "1XPTgDRhN8RFnzniWCddobD9iKZatrvH4"
+
+# Used in later exercises
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}

a568e92db82a23684ad11dec52a565c66d4f56eb
Jamiel Sheikh <i@jamiel.net>
2018-07-30 15:57:03 -0400

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -1,3 +1,30 @@
+        conbaseProRequest = `
+    {
+        "type": "subscribe",
+        "product_ids": [
+            "ETH-USD"
+        ],
+        "channels": [
+            "level2",
+            "ticker",
+            {
+                "name": "ticker",
+                "product_ids": [
+                    "ETH-USD"
+                ]
+            }
+        ]
+    }`
+    
+    
+    
+    bitfinexRequest = `
+    {
+        "event":"subscribe",
+        "channel":"book",
+        "symbol":"ETHUSD"
+     }
+
     pizzaBlockHash = "00000000152340ca42227603908689183edc47355204e7aca59383b0aaac1fd8";
     pizzaTxHash = "a1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d";
     
